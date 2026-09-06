e09819973996ad55a92c153d01b2e25dcc09ad10
Andrew <andrewdakran72@gmail.com>
2026-09-06 12:48:11 +0300

    update

--- src/sms.ts
+++ src/sms.ts
@@ -83,7 +83,7 @@ export async function sendOtp(
   const payload = {
     UserName: credentials.username,
     Password: credentials.password,
-    SMSText: `Your verification is ${otp}. Do not share it with anyone.`,
+    SMSText: `Your verification code is ${otp}. Do not share it with anyone.`,
     SMSLang: "E",
     SMSSender: credentials.sender,
     SMSReceiver: receiver,
